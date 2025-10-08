58c77a37c4cdbd5ee90f909c01c701ba5dc9daca
jiyaaah <jiyahona23@gmail.com>
2025-10-08 15:37:09 +0545

    feat: second version

--- index4.js
+++ index4.js
@@ -1,13 +1,10 @@
-// index3.js - main branch
+// index3.js - rohan3 branch
 // Greeting Program
-console.log("Welcome to the Greeting Program!");
-
-let name = prompt("Enter your name:");
-console.log("Hello, " + name + "!");
+console.log("Welcome, " + prompt("Enter your name:") + "!");
 
 // Math operation
 let num1 = 10;
 let num2 = 5;
 let result = num1 + num2;
-console.log("Result:", result);
+console.log("Sum is:", result);
 
